41fb0857b10599c007e221b19e3193afb4731b8a
SmokeyStack <seansyahril04@gmail.com>
2024-12-26 20:34:17 -0500

    Used nuxt for tauri

---
+++ tauri/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    ssr: false,
+    devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+    vite: {
+        clearScreen: false,
+        envPrefix: ['VITE_', 'TAURI_'],
+        server: { strictPort: true }
+    }
+});
