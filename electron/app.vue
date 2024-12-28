59d71f0980c88e75903e61ed4926048fb70fcb1b
SmokeyStack <seansyahril04@gmail.com>
2024-12-28 14:35:00 -0500

    Improved UI

--- electron/app.vue
+++ electron/app.vue
@@ -1,41 +1,72 @@
 <template>
-    <Menubar>
-        <MenubarMenu>
-            <MenubarTrigger>File</MenubarTrigger>
-            <MenubarContent>
-                <MenubarItem> To be implemented </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-            <MenubarTrigger>Edit</MenubarTrigger>
-            <MenubarContent>
-                <MenubarItem> To be implemented </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-            <MenubarTrigger>Help</MenubarTrigger>
-            <MenubarContent>
-                <MenubarItem @click="openGithub">
-                    Visit Github Repository
-                </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-    </Menubar>
-    <div class="p-4">
-        <div><Button @click="refreshData">Refresh Data</Button></div>
-        <div>
-            <div v-if="loading">Loading...</div>
-            <div v-else>
-                <div v-if="output.length">
-                    <div
-                        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-7 gap-4">
-                        <div v-for="image in output" :key="image">
-                            <img :src="image" :alt="image" />
+    <div class="h-screen flex flex-col">
+        <Menubar>
+            <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem> To be implemented </MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Edit</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem> To be implemented </MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Help</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem @click="openGithub">
+                        Visit Github Repository
+                    </MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+        </Menubar>
+        <div class="flex-grow overflow-hidden">
+            <ResizablePanelGroup direction="horizontal" class="h-full">
+                <ResizablePanel class="h-full" :min-size="21">
+                    <ScrollArea class="h-full rounded-md border p-4">
+                        <div class="p-4 h-full overflow-auto">
+                            <div>
+                                <Button @click="refreshData">
+                                    Refresh Data
+                                </Button>
+                            </div>
+                            <div>
+                                <div v-if="loading">Loading...</div>
+                                <div
+                                    v-else-if="output.length"
+                                    class="grid grid-cols-custom-3 gap-4">
+                                    <div
+                                        v-for="image in output"
+                                        :key="image"
+                                        class="relative w-full h-64 overflow-hidden">
+                                        <img
+                                            :src="image"
+                                            :alt="image"
+                                            class="absolute inset-0 w-full h-full object-cover filter blur-lg" />
+                                        <!-- Centered foreground image -->
+                                        <div
+                                            class="absolute inset-0 flex justify-center items-center">
+                                            <img
+                                                :src="image"
+                                                :alt="image"
+                                                class="object-contain" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div v-else>No images available</div>
+                            </div>
                         </div>
+                    </ScrollArea>
+                </ResizablePanel>
+                <ResizableHandle with-handle />
+                <ResizablePanel class="h-full" :min-size="21">
+                    <div class="h-full p-4 flex items-center justify-center">
+                        Two
                     </div>
-                </div>
-                <div v-else>No images available</div>
-            </div>
+                </ResizablePanel>
+            </ResizablePanelGroup>
         </div>
     </div>
 </template>
@@ -47,10 +78,14 @@
         MenubarContent,
         MenubarItem,
         MenubarMenu,
-        MenubarSeparator,
-        MenubarShortcut,
         MenubarTrigger
     } from '@/components/ui/menubar';
+    import {
+        ResizableHandle,
+        ResizablePanel,
+        ResizablePanelGroup
+    } from '@/components/ui/resizable';
+    import { ScrollArea } from '@/components/ui/scroll-area';
 
     import { ref } from 'vue';
 
--- tauri/app.vue
+++ tauri/app.vue
@@ -1,41 +1,72 @@
 <template>
-    <Menubar>
-        <MenubarMenu>
-            <MenubarTrigger>File</MenubarTrigger>
-            <MenubarContent>
-                <MenubarItem> To be implemented </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-            <MenubarTrigger>Edit</MenubarTrigger>
-            <MenubarContent>
-                <MenubarItem> To be implemented </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-            <MenubarTrigger>Help</MenubarTrigger>
-            <MenubarContent>
-                <MenubarItem @click="openGithub">
-                    Visit Github Repository
-                </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-    </Menubar>
-    <div class="p-4">
-        <div><Button @click="refreshData">Refresh Data</Button></div>
-        <div>
-            <div v-if="loading">Loading...</div>
-            <div v-else>
-                <div v-if="output.length">
-                    <div
-                        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-7 gap-4">
-                        <div v-for="image in output" :key="image">
-                            <img :src="image" :alt="image" />
+    <div class="h-screen flex flex-col">
+        <Menubar>
+            <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem> To be implemented </MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Edit</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem> To be implemented </MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Help</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem @click="openGithub">
+                        Visit Github Repository
+                    </MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+        </Menubar>
+        <div class="flex-grow overflow-hidden">
+            <ResizablePanelGroup direction="horizontal" class="h-full">
+                <ResizablePanel class="h-full" :min-size="21">
+                    <ScrollArea class="h-full rounded-md border p-4">
+                        <div class="p-4 h-full overflow-auto">
+                            <div>
+                                <Button @click="refreshData">
+                                    Refresh Data
+                                </Button>
+                            </div>
+                            <div>
+                                <div v-if="loading">Loading...</div>
+                                <div
+                                    v-else-if="output.length"
+                                    class="grid grid-cols-custom-3 gap-4">
+                                    <div
+                                        v-for="image in output"
+                                        :key="image"
+                                        class="relative w-full h-64 overflow-hidden">
+                                        <img
+                                            :src="image"
+                                            :alt="image"
+                                            class="absolute inset-0 w-full h-full object-cover filter blur-lg" />
+                                        <!-- Centered foreground image -->
+                                        <div
+                                            class="absolute inset-0 flex justify-center items-center">
+                                            <img
+                                                :src="image"
+                                                :alt="image"
+                                                class="object-contain" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div v-else>No images available</div>
+                            </div>
                         </div>
+                    </ScrollArea>
+                </ResizablePanel>
+                <ResizableHandle with-handle />
+                <ResizablePanel class="h-full" :min-size="21">
+                    <div class="h-full p-4 flex items-center justify-center">
+                        Two
                     </div>
-                </div>
-                <div v-else>No images available</div>
-            </div>
+                </ResizablePanel>
+            </ResizablePanelGroup>
         </div>
     </div>
 </template>
@@ -47,14 +78,19 @@
         MenubarContent,
         MenubarItem,
         MenubarMenu,
-        MenubarSeparator,
-        MenubarShortcut,
         MenubarTrigger
     } from '@/components/ui/menubar';
+    import {
+        ResizableHandle,
+        ResizablePanel,
+        ResizablePanelGroup
+    } from '@/components/ui/resizable';
+    import { ScrollArea } from '@/components/ui/scroll-area';
 
-    import { invoke, convertFileSrc } from '@tauri-apps/api/core';
+    import * as core from '@tauri-apps/api/core';
     import * as dialog from '@tauri-apps/plugin-dialog';
     import * as shell from '@tauri-apps/plugin-shell';
+
     import { ref } from 'vue';
 
     const openGithub = () => {
@@ -67,7 +103,7 @@
         loading.value = true;
 
         try {
-            invoke('start_time').then((message: any) =>
+            core.invoke('start_time').then((message: any) =>
                 console.log(`App started at ${message}`)
             );
             const file_path: string | null = await dialog.open({
@@ -78,7 +114,7 @@
                 query: { data: file_path }
             });
             data.value = data.value!.map((image: string) => {
-                return convertFileSrc(image);
+                return core.convertFileSrc(image);
             });
             output.value = data.value!;
         } catch (error) {
