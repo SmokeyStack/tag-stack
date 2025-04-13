865e9c380be27d1133de12bc20c7e9cc3a9ac5aa
SmokeyStack <seansyahril04@gmail.com>
2025-04-13 17:14:23 -0400

    Tauri - Upgraded to ShadcnNuxt v2.0.1

--- tauri/nuxt.config.ts
+++ tauri/nuxt.config.ts
@@ -1,24 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
     ssr: false,
     devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+    css: ['~/assets/css/tailwind.css'],
     vite: {
+        plugins: [tailwindcss()],
         clearScreen: false,
         envPrefix: ['VITE_', 'TAURI_'],
         server: { strictPort: true }
     },
-    modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+    modules: ['shadcn-nuxt', '@nuxtjs/color-mode'],
     shadcn: {
-        /**
-         * Prefix for all the imported component
-         */
         prefix: '',
-        /**
-         * Directory that the component lives in.
-         * @default "./components/ui"
-         */
         componentDir: './components/ui'
     },
     colorMode: {
