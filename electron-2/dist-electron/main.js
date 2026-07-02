852487e8c4a35e02e3a92edb65f43233f816529a
SmokeyStack <84206875+SmokeyStack@users.noreply.github.com>
2026-07-02 19:01:36 -0400

    Building your First App step

--- electron-2/dist-electron/main.js
+++ electron-2/dist-electron/main.js
@@ -1,7 +1,18 @@
 import { BrowserWindow as e, app as t } from "electron";
 //#region electron/main.ts
-t.whenReady().then(() => {
-	new e().loadURL(process.env.VITE_DEV_SERVER_URL);
+function n() {
+	let t = new e({
+		width: 1920,
+		height: 1080,
+		center: !0,
+		autoHideMenuBar: !0
+	});
+	process.env.VITE_DEV_SERVER_URL ? t.loadURL(process.env.VITE_DEV_SERVER_URL) : t.loadFile(path.join(process.env.VITE_PUBLIC, "index.html"));
+}
+t.on("window-all-closed", () => {
+	process.platform !== "darwin" && t.quit();
+}), t.whenReady().then(() => {
+	n();
 });
 //#endregion
 export {};
--- electron-2/electron/main.ts
+++ electron-2/electron/main.ts
@@ -1,5 +1,28 @@
 import { app, BrowserWindow } from 'electron';
 
+function createWindow() {
+    const window = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        center: true,
+        autoHideMenuBar: true
+    });
+
+    if (process.env.VITE_DEV_SERVER_URL) {
+        window.loadURL(process.env.VITE_DEV_SERVER_URL);
+    } else {
+        window.loadFile(path.join(process.env.VITE_PUBLIC!, 'index.html'));
+    }
+}
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
 app.whenReady().then(() => {
-    new BrowserWindow().loadURL(process.env.VITE_DEV_SERVER_URL);
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
