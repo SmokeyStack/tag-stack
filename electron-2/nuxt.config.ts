95974a53f28b12adb2f0f3666dde42199949cfd8
SmokeyStack <84206875+SmokeyStack@users.noreply.github.com>
2026-07-02 18:54:46 -0400

    Added Shadcn-Nuxt

--- electron-2/nuxt.config.ts
+++ electron-2/nuxt.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
+    css: ['~/assets/css/tailwind.css'],
     devtools: { enabled: true },
-    modules: ['nuxt-electron'],
     electron: {
         build: [
             {
@@ -11,5 +13,13 @@ export default defineNuxtConfig({
             }
         ],
         disableDefaultOptions: true
+    },
+    modules: ['nuxt-electron', 'shadcn-nuxt'],
+    shadcn: {
+        prefix: '',
+        componentDir: '@/components/ui'
+    },
+    vite: {
+        plugins: [tailwindcss()]
     }
 });
